e3f4456071b8dbdf1d9dba7107da139d8a5e6b0d
Brycen Dorsay <dorsay@live.ca>
2017-03-15 16:32:06 -0700

    $workers

--- vultr/swarm-init.sh
+++ vultr/swarm-init.sh
@@ -19,7 +19,7 @@ docker-machine create \
   manager1
 
 # Create the swarm workers
-for i in {1..workers} ; do
+for i in {1..$workers} ; do
   docker-machine create \
     -d vultr \
     --vultr-api-key=$VULTR_TOKEN \
