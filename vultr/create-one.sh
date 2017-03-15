8ba4e2da4f0a3f225e9bf207f8d2c53d5ddc564a
Brycen Dorsay <dorsay@live.ca>
2017-03-15 14:23:56 -0700

    fix create one

--- vultr/create-one.sh
+++ vultr/create-one.sh
@@ -6,7 +6,7 @@ docker-machine create \
   --vultr-api-key=$VULTR_TOKEN \
   --vultr-region-id 4 \
   --vultr-plan-id 201 \
-  --vultr-os-id 17
-  --vultr-ssh-user "root"
-  --vultr-ssh-key-id "58bdd9862d752"
+  --vultr-os-id 17 \
+  --vultr-ssh-user "root" \
+  --vultr-ssh-key-id "58bdd9862d752" \
   worker$i
